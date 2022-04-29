870a52f0269f4d0c454b9661635399d0351b8fe5
Kleshcheva13 <klescheva1307@gmail.com>
2022-04-29 22:56:08 +0300

    Изменен способ обработки данных и метрики.

--- TargetNetwork/classify.py
+++ TargetNetwork/classify.py
@@ -34,9 +34,7 @@ raw_test_ds = tf.keras.utils.text_dataset_from_directory(
 
 def custom_standardization(input_data):
   lowercase = tf.strings.lower(input_data)
-  return tf.strings.regex_replace(lowercase,
-                                  ',',
-                                  ' ')
+  return tf.strings.regex_replace(lowercase, ',', ' ')
 
 max_features = 10000
 sequence_length = 250
@@ -75,21 +73,27 @@ model = tf.keras.Sequential([
 
 model.summary()
 
-model.compile(loss=losses.BinaryCrossentropy(from_logits=True),
-              optimizer='adam',
-              metrics=tf.metrics.BinaryAccuracy(threshold=0.0))
+model.compile(
+  loss=losses.BinaryCrossentropy(from_logits=True),
+  optimizer='sgd',
+  metrics=[
+    tf.keras.metrics.AUC(from_logits=True),
+    tf.keras.metrics.BinaryAccuracy(threshold=0.0)])
 
-epochs = 10
+epochs = 500
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs)
 
-loss, accuracy = model.evaluate(test_ds)
+loss, auc, binary_accuracy = model.evaluate(test_ds)
 
-print("Model loss: ", loss)
-print("Model accuracy: ", accuracy)
+print('Export model loss: ', loss)
+print('Export model auc: ', auc)
+print('Export model accaracy: ', binary_accuracy)
 
+
+# Графики
 history_dict = history.history
 history_dict.keys()
 
@@ -97,8 +101,38 @@ acc = history_dict['binary_accuracy']
 val_acc = history_dict['val_binary_accuracy']
 loss = history_dict['loss']
 val_loss = history_dict['val_loss']
-
+auc = history_dict['auc']
+val_auc = history_dict['val_auc']
 epochs = range(1, len(acc) + 1)
+
+# График потерь
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.grid(axis = 'both')
+plt.legend()
+plt.show()
+
+# График точности
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.grid(axis = 'both')
+plt.legend(loc='lower right')
+plt.show()
+
+# График AUC
+plt.plot(epochs, auc, 'r', label='Training auc')
+plt.plot(epochs, val_auc, 'b', label='Validation auc')
+plt.xlabel('Epochs')
+plt.ylabel('AUC')
+plt.grid(axis = 'both')
+plt.legend()
+plt.show()
 
 export_model = tf.keras.Sequential([
   vectorize_layer,
@@ -108,27 +142,41 @@ export_model = tf.keras.Sequential([
 
 export_model.compile(
   loss=losses.BinaryCrossentropy(from_logits=False),
-  optimizer="adam",
-  metrics=['accuracy']
-)
+  optimizer="sgd",
+  metrics=tf.keras.metrics.AUC())
 
 # Test it with `raw_test_ds`, which yields raw strings
-loss, accuracy = export_model.evaluate(raw_test_ds)
-print('Export model loss', loss)
-print('Export model accaracy: ', accuracy)
+loss, auc = export_model.evaluate(raw_test_ds)
+print('')
+print('Export model loss: ', loss)
+print('Export model auc: ', auc)
+print('')
 
-examples = [ 
-  # normal
-  '16:53:55,172.16.0.253,s410,00:00:BE:12:5C:E6,http://tv.tvsat.by:30000/BT1_HD/,3232235626,24,3232235521,45158287,0,0,1,30127,1315112,0,16738,,7778802,0,0,1649426037,1649426037,46,0,0,2,0,0,294',
-  # anomaly
-  '16:52:02,172.28.30.15,s410,00:00:BE:16:0A:76,http://tv.tvsat.by:30000/NTV_B/,3232235623,24,3232235521,581375211,0,0,3197,69045512,2408888987,0,34813421,,6427425,80373959,1,1649417829,1649425922,379426,0,0,202326,1,0,319',
-  # normal
-  '16:53:28,172.23.85.17,s410,00:00:BE:11:D0:73,nil,3232235627,24,3232235521,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil',
-  # normal
-  '16:53:27,172.16.0.253,s615,00:00:BE:37:0A:1C,nil,3232235620,24,3232235521,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil',
-  # anomaly
-  '16:52:16,172.23.146.9,s530,00:00:BE:34:C4:48,http://tv.kottv.by:30002/NatGeoWildHD/,3232235621,24,3232235521,3611056204,0,0,22182,619307491,2594463565,0,163384904,,6331415,82632547,26,1649423584,1649425936,98194,1,1,58799,37889,37889,606']
-
-print('Result:\n', export_model.predict(examples))
 print('Class name 0: ', raw_test_ds.class_names[0])
 print('Class name 1: ', raw_test_ds.class_names[1])
+
+import sys
+
+def get_line_count(file_name):
+  i = 0
+  file = open(file_name, 'r')
+  for line in file:
+    i += 1
+  file.close()
+  return i
+
+if len(sys.argv) == 3:
+  i = 0
+  count = get_line_count(sys.argv[1])
+
+  input_f = open(sys.argv[1], 'r')
+  output_f = open(sys.argv[2], 'w+')
+
+  for line in input_f:
+    print('[', sys.argv[1], '] Line: ', i, '/', count)
+    i += 1
+    output_f.write('[String] ' +  line)
+    output_f.write('[Result] ' +  str(export_model.predict([line])[0]))
+    output_f.write('\n\n')
+  input_f.close()
+  output_f.close()
